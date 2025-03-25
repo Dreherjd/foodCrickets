005b1cf2a6a78ed7388d3c59253687d33d8e9e30
Dreherjd <dreherjd71@gmail.com>
2025-03-25 11:31:25 -0400

    got wysiwyg to work, and file upload component to render, working through submission error

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -104,12 +104,5 @@
         {{ $slot }}
 
         @fluxScripts
-        {{-- @push('styles')
-    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-@endpush
-
-@push('scripts')
-    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
-@endpush --}}
     </body>
 </html>
--- resources/views/livewire/posts/tinymce.blade.php
+++ resources/views/livewire/posts/tinymce.blade.php
@@ -1,31 +1,85 @@
 <?php
 
 use Livewire\Volt\Component;
+use Spatie\LivewireFilepond\WithFilePond;
 
 new class extends Component {
-    public $message;
+    use WithFilePond;
+    public $file;
+    public $title;
+    public $content;
+    public $rating;
+    public $dollar_rating;
+    public $would_go_back;
+    public $hall_of_fame;
+    public $business_name;
+    public $business_addr;
+    public $alt_text;
+
+    public function rules(): array
+    {
+        return [
+            'file' => 'mimetypes:image/jpg,image/jpeg,image/png',
+        ];
+    }
+
+    public function validateUploadedFile()
+    {
+        $this->validate();
+        return true;
+    }
+
+    public function submit()
+    {
+        $validated = $this->validate([
+            'title' => ['required', 'string', 'min:5'],
+            'content' => ['required', 'string', 'min:20'],
+            'rating' => ['required', 'string', 'min:1'],
+            'dollar_rating' => ['string'],
+            'business_name' => ['string', 'min:3'],
+            'business_addr' => ['string'],
+            'alt_text' => ['string', 'min:2'],
+        ]);
+        auth()
+            ->user()
+            ->posts()
+            ->create([
+                'title' => $this->title,
+                'content' => $this->content,
+                'rating' => $this->rating,
+                'dollar_rating' => $this->dollar_rating,
+                'would_go_back' => $this->would_go_back,
+                'hall_of_fame' => $this->hall_of_fame,
+                'business_name' => $this->business_name,
+                'business_addr' => $this->business_addr,
+                'alt_text' => $this->alt_text,
+                'file' => $this->file,
+            ]);
+        redirect(route('dashboard'));
+    }
 }; ?>
 
-@push('styles')
-    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-@endpush
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/3.2.7/css/froala_editor.pkgd.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/3.2.7/js/froala_editor.pkgd.min.js"></script>
 
-@push('scripts')
-    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
-@endpush
-
-<div class="mt-2 bg-white" wire:ignore>
-  <div
-       x-data
-       x-ref="quillEditor"
-       x-init="
-         quill = new Quill($refs.quillEditor, {theme: 'snow'});
-         quill.on('text-change', function () {
-           $dispatch('input', quill.root.innerHTML);
-         });
-       "
-       wire:model.debounce.2000ms="message"
-  >
-    {!! $message !!}
-  </div>
+<div>
+    <form wire:submit='submit' class="space-y-4">
+        <p class="text-xl">Title</p>
+        <x-input wire:model='title' />
+        <x-textarea wire:model='content' id="editor" label="Post Content" />
+        <x-input wire:model='rating' label="Rating" />
+        <x-input wire:model='dollar_rating' label="Dollar Rating" />
+        <x-checkbox wire:model='would_go_back' label="Would Go Back?" />
+        <x-checkbox wire:model='hall_of_fame' label="Hall of Fame" />
+        <x-input wire:model='business_name' label="Business Name" />
+        <x-input wire:model='business_addr' label="Business Address" />
+        <x-input wire:model='alt_text' label="Image Alt Text" />
+        <x-filepond::upload wire:model='file' class="mt-4" multiple="false" wire:model="file" />
+        @filepondScripts
+        <x-button label="Submit" right-icon="arrow-right" type="submit" style="background-color:var(--color-primary);"
+            spinner />
+    </form>
 </div>
+<script>
+    new FroalaEditor('#editor');
+</script>
